c1082330622833bf71a6dab432f02da582732142
kirilkin.97 <kirilkin.97@gmail.com>
2019-04-28 12:17:31 +0300

    Add further configuration for Identity Framework functionality via services and repositories

--- Prikhodko.NewsWebsite/Prikhodko.NewsWebsite.Config/ContainerBuilderExtensions.cs
+++ Prikhodko.NewsWebsite/Prikhodko.NewsWebsite.Config/ContainerBuilderExtensions.cs
@@ -4,7 +4,11 @@ using Prikhodko.NewsWebsite.Data.Contracts.Interfaces;
 using System.Web;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
 using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.DataProtection;
+using Owin;
 using Prikhodko.NewsWebsite.Data.EntityFramework;
 using Prikhodko.NewsWebsite.Data.EntityFramework.Repositories;
 using Prikhodko.NewsWebsite.Service;
@@ -16,10 +20,15 @@ namespace Prikhodko.NewsWebsite.Config
     {
         public static ContainerBuilder AddDataDependencies(this ContainerBuilder builder)
         {
+            builder.Register(c => new IdentityFactoryOptions<ApplicationUserManager>()
+            {
+                DataProtectionProvider = new Microsoft.Owin.Security.DataProtection.DpapiDataProtectionProvider("NewsWebsite")
+            }).As<IdentityFactoryOptions<ApplicationUserManager>>();
+            builder.Register(c => HttpContext.Current.GetOwinContext()).As<IOwinContext>();
             builder.RegisterType<ApplicationDbContext>().AsSelf().InstancePerLifetimeScope();
             builder.Register(c => new UserStore<ApplicationUser>(c.Resolve<ApplicationDbContext>())).As<IUserStore<ApplicationUser>>();
             builder.Register(c => HttpContext.Current.GetOwinContext().Authentication).As<IAuthenticationManager>();
-            builder.RegisterType<ApplicationUserManager>().AsSelf().InstancePerLifetimeScope();
+            builder.RegisterType<ApplicationUserManager>().As<ApplicationUserManager>().InstancePerLifetimeScope();
             builder.RegisterType<ApplicationSignInManager>().AsSelf().InstancePerLifetimeScope();
             builder.RegisterType<ApplicationUserRepository>().As<IRepository<ApplicationUser>>();
             builder.RegisterType<LoginRepository>().As<ILoginRepository>();
--- Prikhodko.NewsWebsite/Prikhodko.NewsWebsite.Web/App_Start/Startup.Auth.cs
+++ Prikhodko.NewsWebsite/Prikhodko.NewsWebsite.Web/App_Start/Startup.Auth.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.DataProtection;
 using Owin;
 using Prikhodko.NewsWebsite.Web.Models;
 
@@ -11,6 +12,7 @@ namespace Prikhodko.NewsWebsite.Web
     public partial class Startup
     {
         // For more information on configuring authentication, please visit https://go.microsoft.com/fwlink/?LinkId=301864
+
         public void ConfigureAuth(IAppBuilder app)
         {
             // Configure the db context, user manager and signin manager to use a single instance per request
